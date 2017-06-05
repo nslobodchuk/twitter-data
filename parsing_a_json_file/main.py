a7e6e1352c0f19b28477cd492723d5a888e3ac31
Nikolay Slobodchuk <nickslobodchuk@gmail.com>
2017-06-05 23:41:12 +0300

    Added functionality to parse first n lines. Network visualization using canvas.

---
+++ parsing_a_json_file/main.py
@@ -0,0 +1,8 @@
+from twitter_parser import parse_tweets
+
+parse_tweets(1000, "graph_1k.json")
+parse_tweets(10000, "graph_10k.json")
+parse_tweets(100000, "graph_100k.json")
+parse_tweets(1000000, "graph_1M.json")
+
+
